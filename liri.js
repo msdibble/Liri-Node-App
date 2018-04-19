4716cbf2144cee578db6448fe5c3a744b44b49a5
unknown <mdibble93@gmail.com>
2018-04-18 21:38:54 -0400

    added some console.logs

--- liri.js
+++ liri.js
@@ -38,4 +38,23 @@ function findSong () {
 var request = require("request");
 
 // Requesting OMDB api
+var getMovie = function(movie) {
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&plot=short&tomatoes=true&apikey=trilogy";
 
+    request(queryURL, function(error, response, body){
+
+        // If the request is successful
+        if(!error && response.statusCode === 200) {
+
+            console.log("Title: " );
+            console.log("Release Year: ");
+            console.log("IMDB Rating: ");
+            console.log("Rotten Tomatoes Rating: ");
+            console.log("Country: ");
+            console.log("Language: ");
+            console.log("Plot: ");
+            console.log("Actors: ");
+        }
+
+    });
+}
